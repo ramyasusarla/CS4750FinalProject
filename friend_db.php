a6693ae8f695885cbdcfb3353c16b4d296b97d36
kca4ys <64979459+kca4ys@users.noreply.github.com>
2022-03-24 17:22:28 -0400

    update friend.php

--- friend_db.php
+++ friend_db.php
@@ -1,17 +1,25 @@
 <?php
 
-function addFriend($name,$major, $year)
+function addFriend($name, $major, $year)
 {
 	// db handler
 	global $db;
 
 	// write sql
+	// insert into friends (name, major, year) values('someone', 'cs', 4)"; 
+	// or
 	// insert into friends values('someone', 'cs', 4)";
-	//$query = "insert into friends values('" . $name . "', '" . $major . "'," . $year . ")";
+
+// bad practice (but convenient)
+	// $query = "insert into friends values('" . $name . "', '" . $major . "'," . $year . ")";
+
+// good practice: use a prepared statement 
+// 1. prepare
+// 2. bindValue & execute	
 	$query = "insert into friends values(:name, :major, :year)";
 
 	// execute the sql
-	//$statement = $db->query($query);   // query() will compile and execute the sql
+	// $statement = $db->query($query);   // query() will compile and execute the sql
 	$statement = $db->prepare($query);
 
 	$statement->bindValue(':name', $name);
@@ -19,7 +27,7 @@ function addFriend($name,$major, $year)
 	$statement->bindValue(':year', $year);
 
 	$statement->execute();
-	
+
 	// release; free the connection to the server so other sql statements may be issued 
 	$statement->closeCursor();
 }
@@ -28,18 +36,18 @@ function getAllFriends()
 {
 	global $db;
 	$query = "select * from friends";
-	//$statement = $db->query($query);     // 16-Mar, stopped here, still need to fetch and return the result 
 
-	//1. prepare
-	//2. bindValue & execute
-	$statement=$db->prepare($query);
-	$statement -> execute();
+// bad	
+	// $statement = $db->query($query);     // 16-Mar, stopped here, still need to fetch and return the result 
 	
-	//$result2=$statement->get_result();
-	//$results=$result2->fetchAll();
+// good: use a prepared stement 
+// 1. prepare
+// 2. bindValue & execute
+	$statement = $db->prepare($query);
+	$statement->execute();
+
 	// fetchAll() returns an array of all rows in the result set
 	$results = $statement->fetchAll();   
-	
 
 	$statement->closeCursor();
 
@@ -50,25 +58,41 @@ function getFriend_byName($name)
 {
 	global $db;
 	$query = "select * from friends where name = :name";
-	//$statement = $db->query($query);     // 16-Mar, stopped here, still need to fetch and return the result 
-
-	//1. prepare
-	//2. bindValue & execute
-	$statement=$db->prepare($query);
+	// "select * from friends where name = $name";
+	
+// 1. prepare
+// 2. bindValue & execute
+	$statement = $db->prepare($query);
 	$statement->bindValue(':name', $name);
-	$statement -> execute();
-	
-	// fetchAll() returns an array of all rows in the result set
+	$statement->execute();
+
+	// fetch() returns a row
 	$results = $statement->fetch();   
-	
 
 	$statement->closeCursor();
 
-	return $results;
+	return $results;	
 }
 
+function updateFriend($name, $major, $year)
+{
+	global $db;
+	$query = "update friends set major=:major, year=:year where name=:name";
+	$statement = $db->prepare($query); 
+	$statement->bindValue(':major', $major);
+	$statement->bindValue(':year', $year);
+	$statement->bindValue(':name', $name);
+	$statement->execute();
+	$statement->closeCursor();
+}
 
-
-
-
+function deleteFriend($name)
+{
+	global $db;
+	$query = "delete from friends where name=:name";
+	$statement = $db->prepare($query); 
+	$statement->bindValue(':name', $name);
+	$statement->execute();
+	$statement->closeCursor();
+}
 ?>
